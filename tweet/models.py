418400e4b8f0c28c92a409bb378e7d847c198b57
KamranAshraf10 <kamranashraf970920@gmail.com>
2025-06-27 23:42:16 +0500

    Login Signup added

--- tweet/models.py
+++ tweet/models.py
@@ -6,9 +6,9 @@ from django.contrib.auth.models import User
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length=240)
-    photo = models.ImageField(upload_to='photos/', blank=True)
+    photo = models.ImageField(upload_to="photos/", blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return  f'{self.user.username} - {self.text[:10]}'
+        return f"{self.user.username} - {self.text[:10]}"
